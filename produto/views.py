52cb319048d22a8cf80964bc00699da9292f5ae9
guilherme.artuzo <guilherme.artuzo@hotmail.com>
2022-04-25 19:56:24 -0300

    cart logic implemented #todo: cart render

--- produto/views.py
+++ produto/views.py
@@ -6,7 +6,9 @@ from django.views.generic.detail import DetailView
 from django.contrib import messages
 
 from django.views import View
+from numpy import quantile
 from . import models
+from pprint import pprint
 
 class ListaProdutos(ListView):
     model = models.Produto
@@ -23,6 +25,12 @@ class DetalheProduto(DetailView):
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
+        
+        #TODO: REMOVER 
+        # if self.request.session.get('carrinho'):
+        #     del self.request.session['carrinho']
+        #     self.request.session.save()
+
         http_referer = self.request.META.get(
             'HTTP_REFERER',
             reverse('produto:lista')
@@ -37,6 +45,29 @@ class AdicionarAoCarrinho(View):
             return redirect(http_referer)
         
         variacao = get_object_or_404(models.Variacao, id=variacao_id)
+        variacao_estoque = variacao.estoque
+        produto = variacao.produto
+
+        produto_id = produto.id
+        produto_nome = produto.nome
+        variacao_nome = variacao.nome or ''
+        preco_unitario = variacao.preco
+        preco_unitario_promocional = variacao.preco_promocional
+        qunatidade = 1
+        slug = produto.slug
+        imagem = produto.imagem
+
+        if imagem:
+            imagem = imagem.name
+        else:
+            imagem = ''
+
+        if variacao.estoque < 1:
+            messages.error(
+                self.request,
+                'Estoque insuficiente'
+            )
+            return redirect(http_referer)
 
         if not self.request.session.get('carrinho'):
             self.request.session['carrinho'] = {}
@@ -45,13 +76,43 @@ class AdicionarAoCarrinho(View):
         carrinho = self.request.session['carrinho']
 
         if variacao_id in carrinho:
-            #TODO: Variação existe no carrinho
-            pass
-        else:
-            #TODO: Variação não existe no carrinho
-            pass
+            quantidade_carrinho = carrinho[variacao_id]['quantidade']
+            quantidade_carrinho += 1
 
-        return HttpResponse(f'{variacao.produto} {variacao.nome}')
+            if variacao_estoque < quantidade_carrinho:
+                messages.warning(
+                    self.request,
+                    f'Estoque insuficiente para {quantidade_carrinho} no '
+                    f'produto "{produto_nome}". Adicionamos {variacao_estoque}'
+                    f'no seu carrinho'
+                )
+                quantidade_carrinho = variacao_estoque
+
+            carrinho[variacao_id]['quantidade'] = quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo'] = preco_unitario  * quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo_promocional'] = preco_unitario_promocional  * quantidade_carrinho
+
+        else:
+            carrinho[variacao_id] = {
+                'produto_id' : produto_id,
+                'produto_nome' : produto_nome,
+                'variacao_nome' : variacao_nome,
+                'variacao_id' : variacao_id,
+                'preco_unitario': preco_unitario,
+                'preco_unitario_promocional' : preco_unitario_promocional,
+                'preco_quantitativo': preco_unitario,
+                'preco_quantitativo_promocional' : preco_unitario_promocional,
+                'quantidade' : 1,
+                'slug' : slug,
+                'imagem' : imagem
+            }
+
+        self.request.session.save()
+        messages.success(
+            self.request,
+            f'Produto {produto.nome} {variacao.nome} adicionado ao carrinho !'
+        )
+        return redirect(http_referer)
 
 
 class RemoverDoCarrinho(View):
@@ -59,7 +120,8 @@ class RemoverDoCarrinho(View):
 
 
 class Carrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        return render(self.request, 'produto/carrinho.html')
 
 
 class Finalizar(View):
