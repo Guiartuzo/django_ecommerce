adafce957689b01d3d49c61558e57dc7b6e432a1
guilherme.artuzo <guilherme.artuzo@hotmail.com>
2022-05-03 19:50:46 -0300

    implementation of forms testing

---
+++ tests/tests_forms.py
@@ -0,0 +1,24 @@
+from django.test import SimpleTestCase, TestCase
+from perfil.forms import PerfilForm, UserForm
+from datetime import datetime
+from django.contrib.auth.models import User
+
+
+class TestForms(TestCase):
+
+    def test_perfilform_valid_data(self):
+        form = UserForm(data={
+            'first_name':'test-user',
+            'last_name' : 'test-user-lastname',
+            'username' : 'test-user-username',
+            'password' : '12345',
+            'email' : 'test@email.com',
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_perfilfom_no_data(self):
+        form = PerfilForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors),11)
