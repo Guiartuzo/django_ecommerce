5f0ca93808a4b74f5af04597d6b5fc7b5ec4a4e1
guilherme.artuzo <guilherme.artuzo@hotmail.com>
2022-05-03 17:11:46 -0300

    implementation of view tests

--- tests/tests_views.py
+++ tests/tests_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from produto.models import Produto, Variacao
-import json
+from produto.models import Produto
+
 
 class TestProdutoViews(TestCase):
 
@@ -17,6 +17,7 @@ class TestProdutoViews(TestCase):
         )
         self.carrinho_url = reverse('produto:carrinho')
         self.resumodacompra_url = reverse('produto:resumodacompra')
+
 
     def test_produto_list_GET(self):
         response = self.client.get(self.lista_url)
@@ -45,8 +46,40 @@ class TestProdutoViews(TestCase):
 
 
 class TestPerfilViews(TestCase):
-    pass
+    
+    def setUp(self):
+        self.client = Client()
+        self.criar_url = reverse('perfil:criar')
 
+    def test_criar_GET(self): 
+        response = self.client.get(self.criar_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'perfil/criar.html')
 
 class TestPedidoViews(TestCase):
-    pass
+    
+    def SetUp(self):
+        self.client = Client()
+        self.detalhe_url = reverse('pedido:detalhes')
+        self.pagar_url = reverse('pedido:pagar')
+        self.salvarpedido_url = reverse('pedido:salvarpedido')
+
+    #TODO : uncomment tests as soon as implemented
+    # def test_detalhes_GET(self):
+    #     response = self.client.get(self.detalhe_url)
+
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'pedido/detalhe.html')
+
+    # def test_pagar_GET(self):
+    #     response = self.client.get(self.pagar_url)
+
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'pedido/pagar.html')
+
+    # def test_salverpedido_GET(self):
+    #     response = self.client.get(self.salvarpedido_url)
+
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'pedido/salvarpedido.html')
